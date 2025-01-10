050f61c2ffd78bff491e7d9aebb9bdb3262ed16b
Nurbek <nergashev816@gmail.com>
2025-01-11 00:36:56 +0500

    push

---
+++ vite-project/src/constants/db.js
@@ -0,0 +1,60 @@
+export function getData() {
+	return [
+		{
+			title: 'JavaScript',
+			price: 17.99,
+			Image:
+				'https://smartpro.com.tr/wp-content/uploads/2024/02/javascript.png',
+			id: 1,
+		},
+		{
+			title: 'ReactJS',
+			price: 15,
+			Image:
+				'https://www.itcube.net/assets/img/blog/ReactJs-Development.webp',
+			id: 2,
+		},
+		{
+			title: 'VueJS',
+			price: 13.5,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9_qH5qcAQf4-maj_La6z1GvQ2zXUJpPmasA&s',
+			id: 3,
+		},
+		{
+			title: 'NodeJS',
+			price: 13.99,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRv3PGEyeJbr65Z2XC-eBGIJvQdtpg0L7XkA&s',
+			id: 4,
+		},
+		{
+			title: 'NestJS',
+			price: 12.5,
+			Image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXWOeloDIIHbCdp2obUT7JREqVR2buJr68wA&s',
+			id: 5,
+		},
+		{
+			title: 'NextJS',
+			price: 12.99,
+			Image:
+				'https://media.licdn.com/dms/image/v2/D4D12AQFJWfUQaQ1qPg/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1675674296261?e=2147483647&v=beta&t=P8Jq3VX9Y4XbyaI7Y1CwViZVcW3LzVONrntTaogyL94',
+			id: 6,
+		},
+		{
+			title: 'TypeScript',
+			price: 22.99,
+			Image:
+				'https://blog.skillfactory.ru/wp-content/uploads/2023/02/typescript.png',
+			id: 7,
+		},
+		{
+			title: 'TensorflowJS',
+			price: 28.99,
+			Image:
+				'https://miro.medium.com/v2/resize:fit:1200/1*1iMg5fkBKlMuNXhpuFsdQA.png',
+			id: 8,
+		},
+	];
+}
